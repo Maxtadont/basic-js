f01c21bbb9854e3b1a955004cad0640ba03530f8
Maxtadont <tusaeff@gmail.com>
2020-10-01 20:43:54 +0400

    feat: solve count-cats feat: solve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members) 
+    return false;
+  
+  if (!Array.isArray(members))
+    return false;
+  
+  let newarr = [];
+  for (let el of members) {
+    if (typeof el === "string")
+      newarr.push(el)
+  }
+
+  let str = '';
+  for (let el of newarr) {
+    str += el.split(' ').join('').slice(0,1);
+  }
+  
+  let str2 = '';
+  str2 = str.toUpperCase().split('').sort().join('');
+  return str2;
 };
