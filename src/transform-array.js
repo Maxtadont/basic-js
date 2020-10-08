b0fae6e67733bfe5d6d7fa64037eaeb90a0a7b8d
Maxtadont <tusaeff@gmail.com>
2020-10-08 23:11:46 +0400

    feat: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  const controls = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ]
+  
+  let res = [];
+  if (!array && array.length === 0 && !Array.isArray(array))
+    res = []
+  else {
+    for (let i = 0; i < array.length; i++){      
+      switch (array[i]) {
+        case controls[0]: 
+          if (i < array.length) 
+            i += 2; 
+          break;
+        case controls[1]:
+          if (i > 0)
+            res.pop();
+          break;
+        case controls[2]:
+          if (i < array.length-1)
+            res.push(array[i+1]);
+          break;
+        case controls[3]:
+          if (i > 0)
+            res.push(array[i-1]);
+          break;
+        default: 
+            res.push(array[i]);
+          break;
+      }
+    }
+  }
+  console.log(res);
+  return res;
 };
